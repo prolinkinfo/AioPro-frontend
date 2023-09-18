260cd9f6305598160258792550c3c0feb9b959a6
KrushilProlink <krushil.prolink@gmail.com>
2023-09-18 15:52:46 +0530

    set hierarchy and set favicon icon

---
+++ src/pages/Hierarchy/Edit.js
@@ -0,0 +1,92 @@
+/* eslint-disable arrow-body-style */
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, FormLabel, Grid, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react'
+import ClearIcon from '@mui/icons-material/Clear';
+import DeleteModel from '../../components/Deletemodle'
+
+const Edit = ({ isOpenModel, closeModal }) => {
+
+    const [isOpenDelete, setIsOpenDelete] = useState(false)
+
+    const handleOpenDelete = () => setIsOpenDelete(true)
+    const handleCloseDelete = () => setIsOpenDelete(false)
+
+    return (
+        <div>
+            <DeleteModel isOpenDeleteModel={isOpenDelete} handleCloseDeleteModel={handleCloseDelete}/>
+
+            <Dialog open={isOpenModel} aria-labelledby="scroll-dialog-title" aria-describedby="scroll-dialog-description">
+                <DialogTitle
+                    id="scroll-dialog-title"
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                    }}
+                >
+                    <Typography variant="h6">Edit</Typography>
+                    <Typography>
+                        <ClearIcon onClick={closeModal} style={{ cursor: 'pointer' }} />
+                    </Typography>
+                </DialogTitle>
+
+                <DialogContent dividers>
+                    <form>
+                        {/* <Grid container rowSpacing={3} columnSpacing={{ xs: 0, sm: 5, md: 4 }}>
+                            <Grid item xs={12} sm={12} md={12}>
+                                <FormLabel>Last name</FormLabel>
+                                <TextField
+                                    id="lastName"
+                                    name="lastName"
+                                    label=""
+                                    size="small"
+                                    fullWidth
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={12} md={12}>
+                                <FormLabel>Email</FormLabel>
+                                <TextField
+                                    id="emailAddress"
+                                    name="emailAddress"
+                                    label=""
+                                    size="small"
+                                    fullWidth
+                                />
+                            </Grid>
+
+                        </Grid> */}
+                    </form>
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        type="submit"
+                        variant="contained"
+
+                        style={{ textTransform: 'capitalize' }}
+                    >
+                        Save
+                    </Button>
+                    <Button
+                        type="reset"
+                        variant="contained"
+                        color="error"
+                        style={{ textTransform: 'capitalize' }}
+                        onClick={handleOpenDelete}
+                    >
+                        Delete
+                    </Button>
+                    <Button
+                        type="reset"
+                        variant="outlined"
+                        color="error"
+                        style={{ textTransform: 'capitalize' }}
+                        onClick={closeModal}
+                    >
+                        Cancle
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
+
+export default Edit
--- src/pages/Hierarchy/hierarchy.js
+++ src/pages/Hierarchy/hierarchy.js
@@ -1,17 +1,30 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import ReactFlow, { Background, useNodesState, useEdgesState, addEdge } from 'reactflow';
+import ReactFlow, { Background, useNodesState, useEdgesState, addEdge, Handle, Position } from 'reactflow'
 import ContextMenu from './ContextMenu';
 import 'reactflow/dist/style.css';
 import './style.css';
+import './diagram.css'
 import { allusers } from '../../service/api';
+import EditModel from './Edit'
+// eslint-disable-next-line import/order
+import { useParams } from 'react-router-dom';
 
-const initialNodes = [];
+let initialNodes = []
+let initialEdges = []
 
-const initialEdges = [];
 export const Hierarchy = () => {
+  // const [initialNodes, setInitialNodes] = useState([])
+  // const [initialEdges, setInitialEdges] = useState([])
+  const [data, setData] = useState(null)
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const [menu, setMenu] = useState(null);
+  const [isOpenModel, setIsOpenModel] = useState(false)
+  // const [selectedList, setSelectedList] = useState(null)
+
+  const params = useParams();
+
+  console.log(params.id, "params")
   const ref = useRef(null);
 
   const onConnect = useCallback((params) => setEdges((els) => addEdge(params, els)), [setEdges]);
@@ -35,37 +48,120 @@ export const Hierarchy = () => {
     [setMenu]
   );
 
-  async function fetchdata() {
-    const result = await allusers('/api/users');
+  // function findSourceById(array, targetId) {
+  //   // eslint-disable-next-line no-plusplus
+  //   for (let i = 0; i < array.length; i++) {
+  //     if (array[i].target === targetId) {
+  //       return array[i].source;
+  //     }
+  //   }
+  //   // If no matching item is found, you can return null or handle it as needed.
+  //   return null;
+  // }
 
-    if (result && result.status === 200) {
-      result.data.forEach((item, index) => {
-        initialNodes.push({ id: item?._id, position: { x: 175, y: 0 }, data: { label: item?.firstName } });
-        initialEdges.push({ id: `e1-${index + 1}`, source: item?.parentId, target: item?._id, animated: true });
+  const handleNodeDoubleClick = useCallback((event, node) => {
+    console.log(node, "node")
+    // const source = findSourceById(initialEdges, node?.id);
+    // setSelectedList(...selectedList, { sourceId: source })
+    // console.log(source, "source")
+
+
+    setIsOpenModel(true)
+  }, []);
+
+
+  const closeModal = () => {
+    setIsOpenModel(false)
+  };
+
+  function findItems(data, id) {
+    // Create a set to store unique parent IDs
+    const parentIds = new Set();
+
+    // Find items that match the first condition (_id === id)
+    const itemsMatchingId = data.filter(item => item._id === id);
+
+    // Find items that match the second condition (parentId === id)
+    const itemsMatchingParentId = data.filter(item => item.parentId === id);
+
+    // Add the parent IDs of items matching the second condition to the set
+    itemsMatchingParentId.forEach(item => parentIds.add(item._id));
+
+    // Find items that match the third condition (_id is the parent of another item's parentId)
+    const itemsMatchingParentInAnyItem = data.filter(item => parentIds.has(item.parentId));
+
+    // Combine the results of all three conditions
+    const combinedResults = [...itemsMatchingId, ...itemsMatchingParentId, ...itemsMatchingParentInAnyItem];
+
+    return combinedResults;
+  }
+
+  useEffect(() => {
+    const newNodes = [];
+    const newEdges = [];
+    if (data && data.length > 0) {
+
+      console.log(data, "data", "111111111111111111")
+      data.forEach((item, index) => {
+        newNodes.push({ id: item?._id, position: { x: 175, y: 0 }, data: { label: item?.firstName } });
+        newEdges.push({ id: `e1-${index + 1}`, source: item?.parentId, target: item?._id, animated: true });
       });
     }
+
+    initialNodes = newNodes
+    initialEdges = newEdges
+    // setInitialNodes(newNodes)
+    // setInitialEdges(newEdges)
+    console.log(initialNodes, "initialNodes", "111111111111111111")
+    console.log(initialEdges, "initialEdges", "111111111111111111")
+  }, [data])
+
+  // useEffect(() => {
+  //   if (data && data.length > 0) {
+  //     data.forEach((item, index) => {
+  //       setInitialNodes(...initialNodes, { id: item?._id, position: { x: 175, y: 0 }, data: { label: item?.firstName } });
+  //       setInitialEdges(...initialEdges, { id: `e1-${index + 1}`, source: item?.parentId, target: item?._id, animated: true });
+  //     });
+  //   }
+  // }, [data])
+
+
+  async function fetchdata() {
+    const result = await allusers('/api/users');
+    console.log(params.id, "111111111111111111")
+    setData(findItems(result.data, params.id));
+
   }
 
   useEffect(() => {
     fetchdata();
-  }, []);
+  }, [params]);
+
+
 
   // Close the context menu if it's open whenever the window is clicked.
   const onPaneClick = useCallback(() => setMenu(null), [setMenu]);
+
+
   return (
-    <ReactFlow
-      ref={ref}
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      onPaneClick={onPaneClick}
-      onNodeContextMenu={onNodeContextMenu}
-      fitView
-    >
-      <Background />
-      {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
-    </ReactFlow>
+    <>
+      <EditModel isOpenModel={isOpenModel} closeModal={closeModal} />
+
+      <ReactFlow
+        ref={ref}
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onPaneClick={onPaneClick}
+        onNodeContextMenu={onNodeContextMenu}
+        onNodeDoubleClick={handleNodeDoubleClick}
+        fitView
+      >
+        <Handle type="target" position={Position.Left} />
+        {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
+      </ReactFlow>
+    </>
   );
 };
