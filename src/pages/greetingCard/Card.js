2ab5e838be36728b47a95ebd69e625f7471b1066
KrushilProlink <krushil.prolink@gmail.com>
2023-10-09 10:29:19 +0530

    recreate greeting card page

--- src/pages/greetingCard/Card.js
+++ src/pages/greetingCard/Card.js
@@ -1,41 +1,11 @@
-/* eslint-disable arrow-body-style */
-import { Box, Button, Container, Grid, Stack, Typography } from '@mui/material'
 import React from 'react'
-import TableStyle from '../../components/TableStyle'
-import Iconify from '../../components/iconify/Iconify'
 
 const Card = () => {
-    return (
-        <div>
-            <Container maxWidth="xl">
-                <TableStyle>
-                    <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
-                        <Typography variant="h4">Greeting Card</Typography>
-                        {/* <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}> */}
-                        {/* <Link to="/dashboard/greetingcard/add" style={{ textDecoration: 'none', color: 'white' }}>
-                                New Template
-                            </Link> */}
-                        {/* </Button> */}
-                    </Stack>
-                    <Box width="100%">
-                        <Grid container rowSpacing={3} columnSpacing={{ xs: 0, sm: 5, md: 4 }}>
-                            <Grid item xs={12} sm={6} md={6}>
-                                <Card>
-                                    <h1>1</h1>
-                                </Card>
-                            </Grid>
-                            <Grid item xs={12} sm={6} md={6}>
-                                <Card>
-                                    <h1>2</h1>
-                                </Card>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </TableStyle>
-
-            </Container>
-        </div>
-    )
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
 }
 
 export default Card
--- src/routes.js
+++ src/routes.js
@@ -28,10 +28,10 @@ import { Hierarchy } from './pages/Hierarchy/hierarchy';
 import Notification from './layouts/dashboard/header/Notification';
 import Location from './pages/location';
 import Opd from './pages/opd/Opd'
+import Card from './pages/greetingCard/Card'
 // import Card from './pages/card/CardTemplate'
 // import AddCard from './pages/card/Add'
 // import GreetingCardView from './pages/card/View'
-import Card from './pages/greetingCard/Card'
 // ----------------------------------------------------------------------
 
 export default function Router() {
