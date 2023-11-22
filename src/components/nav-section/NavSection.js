c893e435bcaa728ff64189daae684faafb8e0522
jaysukh22 <jaysukh.specscale@gmail.com>
2023-11-22 11:16:48 +0530

    add sidebar and admin page

--- src/components/nav-section/NavSection.js
+++ src/components/nav-section/NavSection.js
@@ -1,54 +1,184 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { NavLink as RouterLink } from 'react-router-dom';
-// @mui
-import { Box, List, ListItemText } from '@mui/material';
-//
-import { StyledNavItem, StyledNavItemIcon } from './styles';
+import { Icon } from '@iconify/react';
+import { NavLink as RouterLink, matchPath, useLocation } from 'react-router-dom';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+import {
+  Box,
+  List,
+  Collapse,
+  ListItemText,
+  ListItemIcon,
+  ListSubheader,
+  ListItemButton,
+  styled,
+  useTheme,
+  alpha,
+} from '@mui/material';
 
 // ----------------------------------------------------------------------
 
-NavSection.propTypes = {
-  data: PropTypes.array,
-};
+const ListSubheaderStyle = styled((props) => <ListSubheader disableSticky disableGutters {...props} />)(
+  ({ theme }) => ({
+    ...theme.typography.overline,
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(2),
+    paddingLeft: theme.spacing(5),
+    color: theme.palette.text.primary,
+  })
+);
 
-export default function NavSection({ data = [], ...other }) {
-  return (
-    <Box {...other}>
-      <List disablePadding sx={{ p: 1 }}>
-        {data.map((item) => (
-          <NavItem key={item.title} item={item} />
-        ))}
-      </List>
-    </Box>
-  );
-}
+const ListItemStyle = styled((props) => <ListItemButton disableGutters {...props} />)(({ theme }) => ({
+  ...theme.typography.body2,
+  height: 48,
+  position: 'relative',
+  textTransform: 'capitalize',
+  paddingLeft: theme.spacing(5),
+  paddingRight: theme.spacing(2.5),
+  color: theme.palette.text.secondary,
+  '&:before': {
+    top: 0,
+    right: 0,
+    width: 3,
+    bottom: 0,
+    content: "''",
+    display: 'none',
+    position: 'absolute',
+    borderTopLeftRadius: 4,
+    borderBottomLeftRadius: 4,
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
+
+const ListItemIconStyle = styled(ListItemIcon)({
+  width: 22,
+  height: 22,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
 
 // ----------------------------------------------------------------------
 
 NavItem.propTypes = {
+  active: PropTypes.func,
+  isShow: PropTypes.bool,
   item: PropTypes.object,
 };
 
-function NavItem({ item }) {
-  const { title, path, icon, info } = item;
+function NavItem({ item, active, isShow }) {
+  const theme = useTheme();
+  const isActiveRoot = active(item?.path);
+  const { title, path, icon, info, children } = item;
+  const [open, setOpen] = useState(isActiveRoot);
+
+  const handleOpen = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const activeRootStyle = {
+    color: 'primary.main',
+    fontWeight: 'fontWeightMedium',
+    bgcolor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity),
+    '&:before': { display: 'block' },
+  };
+
+  const activeSubStyle = {
+    color: 'text.primary',
+    fontWeight: 'fontWeightMedium',
+  };
+
+  if (children) {
+    return (
+      <>
+        <ListItemStyle
+          onClick={handleOpen}
+          sx={{
+            ...(isActiveRoot && activeRootStyle),
+          }}
+        >
+          <ListItemIconStyle>{icon && icon}</ListItemIconStyle>
+
+          <ListItemText disableTypography primary={title} />
+          {info && info}
+
+          {open ? <ExpandMoreIcon /> : <KeyboardArrowRightIcon />}
+        </ListItemStyle>
+
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            {children?.map((item) => {
+              const { title, path } = item;
+              const isActiveSub = active(path);
+
+              return (
+                <ListItemStyle
+                  key={title}
+                  component={RouterLink}
+                  to={path}
+                  sx={{
+                    ...(isActiveSub && activeSubStyle),
+                  }}
+                >
+                  <ListItemIconStyle>
+                    <Box
+                      component="span"
+                      sx={{
+                        width: 4,
+                        height: 4,
+                        display: 'flex',
+                        borderRadius: '50%',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        bgcolor: 'text.disabled',
+                        transition: (theme) => theme.transitions.create('transform'),
+                        ...(isActiveSub && {
+                          transform: 'scale(2)',
+                          bgcolor: 'primary.main',
+                        }),
+                      }}
+                    />
+                  </ListItemIconStyle>
+                  <ListItemText disableTypography primary={title} />
+                </ListItemStyle>
+              );
+            })}
+          </List>
+        </Collapse>
+      </>
+    );
+  }
 
   return (
-    <StyledNavItem
-      component={RouterLink}
-      to={path}
-      sx={{
-        '&.active': {
-          color: 'text.primary',
-          bgcolor: 'action.selected',
-          fontWeight: 'fontWeightBold',
-        },
-      }}
-    >
-      <StyledNavItemIcon>{icon && icon}</StyledNavItemIcon>
-
+    <ListItemStyle component={RouterLink} to={path}>
+      <ListItemIconStyle>{icon && icon}</ListItemIconStyle>
       <ListItemText disableTypography primary={title} />
-
       {info && info}
-    </StyledNavItem>
+    </ListItemStyle>
+  );
+}
+
+NavSection.propTypes = {
+  isShow: PropTypes.bool,
+  navConfig: PropTypes.array,
+};
+
+export default function NavSection({ data = [], ...other }) {
+  const { pathname } = useLocation();
+  const match = (path) => (path ? !!matchPath({ path, end: false }, pathname) : false);
+
+
+  return (
+    <Box {...other}>
+      {data?.map((list, index) => {
+        return (
+          <List disablePadding key={index}>
+            <NavItem item={list} active={match} />
+          </List>
+        );
+      })}
+    </Box>
   );
 }
